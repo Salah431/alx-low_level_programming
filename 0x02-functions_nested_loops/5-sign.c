3971d86746d120da134fddde7366685ae990616a
Salah <salahayman431@gmail.com>
2023-07-18 11:52:12 -0700

    ccc

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	} else if (n == 0)
 }
@@ -18,7 +18,7 @@ int print_sign(int n)
 		return (0);
 	} else  (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		return (-1);
 	}
 }
